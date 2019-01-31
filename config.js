a5091945eb8ea8252bd115ec04e203bbd9b88656
vishaldeepverma <vishaldeepverma@live.in>
2019-01-31 13:31:35 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  obj: {
+    "Default-Welcome-Intent":
+      "Hi I am Zora, a chatbot, here to assist you. You can ask me about Zorang and it's services. I can also book a demo for you.",
+    "zorang.about":
+      "Zorang is a three-year-old San Francisco bay area startup focusing on IT Strategy, AI, eCommerce and Content Management consulting services for our clients in North America and also developing cutting-edge mobile applications. We partner with industry leaders like SAP Hybris, IBM, PIMCore, Akeneo, Acquia, Jitterbit etc. to provide best of breed solution offerings for our customers.",
+    "zorang.about-AI-work":
+      "We are big on AI and are building custom solutions for our customers leveraging platforms like IBM Watson and Google AI.",
+    "zorang.address":
+      "We are a San Francisco Bay area (silicon valley) based company, with offices in Gurugram and Bengaluru, India",
+    "zorang.askForPhoneNumber": "We will contact you soon!",
+    "zorang.askForPhoneNumber - no": "We will contact you soon",
+    "zorang.blog": "Read our blog on  http://87f.4fb.myftpupload.com/blog/.",
+    "zorang.book-demo": "Should I move ahead to book a demo?",
+    "zorang.book-demo - yes":
+      "Here is what you told me - 'parameter' Do you want to proceed?",
+    "zorang.book-demo - yes - no": "Alright!! Cancelling",
+    "zorang.book-demo - yes - yes": "Please provide me your email address",
+    "zorang.book-demo - yes - yes - custom":
+      "Great!! You have successfully raised a ticket. We will connect with you soon. Thanks for showing your interest",
+    "zorang.book-demo - no": "Not an issue!! Anything else you want to know?",
+    "zorang.BotCanDo":
+      "Hi, I am Zora, a chatbot, here to assist you. You can ask me about Zorang and it 's services. I can also book a demo for you.",
+    "zorang.business-Analysis":
+      "Business Analysis and Strategy is our core competency and we are helping our customers maximize their IT budgets by prioritizing projects that will achieve the biggest ROI.",
+    "zorang.business-Analysis - email": "",
+    "zorang.businessStrategy":
+      "Business Analysis and Strategy is our core competency and we are helping our customers maximize their IT budgets by prioritizing projects that will achieve the biggest ROI. Please provide me your email Id for more info.",
+    "zorang.businessStrategy - email": "",
+    "zorang.client":
+      "We have clients spanning Fortune 500 to startups funded by the leading VCs in the industry.",
+    "zorang.cloud-Integration":
+      "We have extensive experience in IBM AppConnect and have built sophisticated real-time, near real-time and batch integration use cases, connecting applications such as Salesforce in the cloud to on-premise legacy applications such as SAP etc.<br><a> For any further info please provide me your email Id.</a>",
+    "zorang.cloud-Integration - email": "",
+    "zorang.cloudpartners":
+      "We have a lot of friends in the industry and like working with IBM, Akeneo, SAP Hybris, Jitterbit, PIMCore etc.",
+    "zorang.cofounder":
+      "IT Industry veterans, Anurag Gupta and Sumit Kapoor, with over 20 years of experience at companies like IBM, Cognizant etc., founded Zorang, as they believed that with the adoption of cloud customers were looking at a radically new way for IT services.",
+    "zorang.connectToSales":
+      "Would you like further assistance? I will be happy to connect you to our Account Executive who can assist you further.",
+    "zorang.contact-email": "You can reach us at <a>marketing@zorang.com<a>",
+    "zorang.contact-info":
+      "Contact us :   email : sales@zorang.com , Phone: +1 (925) 391-0073 , address : 3494 Camino Tassajara #125, Danville, CA 94506 USA",
+    "zorang.contact-me": "We will contact you soon",
+    "zorang.contact-me - no": "Not an issue",
+    "zorang.contact-me - email": "",
+    "zorang.contacts-phone":
+      "You can contact us at sales@zorang.com or  +1 (925) 391-0073",
+    "zorang.customer":
+      "Cosmo Music :  Specializing in all instruments including guitars, band & orchestral strings, pro audio, home recording studios, keyboards & software, drums & percussion, acoustic & digital pianos as well as print music & vinyl LPs.",
+    "zorang.data-Integration":
+      "Yes, we have extensive experience in Cloud Integration, Enterprise Application Integration, APIs, Micro-services architecture etc. We are also implementation partners of IBM and Jitterbit and also have experience with Dell Boomi among others.Please provide me your email id for more details.",
+    "zorang.data-Integration - email": "",
+    "zorang.data-Integration - custom - yes":
+      "Alright! We have taken a note. We will contact you soon. Thanks Buddy",
+    "zorang.domain":
+      "We have expertise in Enterprise Content Management (ECM), eCommerce solution offerings like Demandware (SFCC), SAP Hybris, WCS, Magento etc, AI Solutions, React/Native Mobile development for Android and iOS, Java, PHP, Python etc., development.",
+    "zorang.DoYouProvideIntegration":
+      "Yes! We do Data Integration , Enterprise Application Integration , Cloud Integration and Hybris consulting",
+    "zorang.ecommerce":
+      "Yes, we have extensive eCommerce experience, deploying custom solutions, SAP Hybris, Demandware/Salesforce Commerce Cloud, IBM WebSphere Commerce Server, Magento solutions.",
+    "zorang.HowOld":
+      "Zorang has a mobile development practice, building and managing iOS and Android apps",
+    "zorang.Hybriswork":
+      "Zorang has extensive experience in Hybris, the leadership and consultants have been working on large hybris implementations for 7+ years. Zorang has executed multiple hybris B2C, B2B and PIM projects for Fortune 500 companies.",
+    "zorang.IBMAppConnect":
+      "We have extensive experience in IBM AppConnect and have built sophisticated real-time, near real-time and batch integration use cases, connecting applications such as Salesforce in the cloud to on-premise legacy applications such as SAP etc.",
+    "zorang.incorporatedDate":
+      "Though we are young, and were founded 3 years back, we have extensive experience under our belts.",
+    "zorang.job":
+      "We are growing rapidly and have multiple openings across our various practices. Please send your resume with a short cover letter to hr@zorang.com. We will contact you shortly. You can also upload your resume here:<a href='https://www.dropbox.com/request/22xNxEo1DyfAhSvTA9c7' target=_blank>Upload Resume and Cover letter!</a>",
+    "zorang.leaveit": "Ok !! Go ahead",
+    "zorang.MobileDevelopment":
+      "Zorang works on its location-based anonymous social networking platform. We have built this app on  Android as well as IOS. You can download Qaddoo from google play store.",
+    "zorang.notFriend":
+      "Oh! I am not a human being. But I love making relationship. I apologies if  you are disheartened by my behavior.",
+    "zorang.OtherCMS":
+      "Zorang founders are CMS veterans and have 20+ years of experience working with Content Management Systems. If you have heard of a CMS system, chances are Zorang has experience with it. Zorang has partnerships and implementation experience with multiple CMS systems like Bloomreach, Adobe AEM, Drupal, Pimcore, Sitecore, SDL Tridion and Opentext (Interwoven).",
+    "zorang.pricing":
+      "Our quotes depend upon your business requirements and we will be glad to work with you to understand the same and provide a competitive and compelling business proposition.  Provide your email Id.",
+    "zorang.pricing - email": "",
+    "zorang.pricing - email - no": "We will contact you soon",
+    "zorang.product":
+      "We are also developing our own products and applications in the product information management, integration and mobile. For example, we have developed a Rules Engine UI for Akeneo, available on the Akeneo Marketplace.",
+    "zorang.projects":
+      "Zorang works on AI, E-Commerce , ECM and PIM related projects.",
+    "zorang.Query": "yeah sure!! Please ask",
+    "zorang.service":
+      "Zorang provides Business Analysis, Cloud Migration, Vendor Evaluation, Solution Architecture, Mobile Development, Cloud Hosting and many more services.",
+    "zorang.ServiceBase":
+      "We are not any services company and pride ourselves in innovation, just ask CIOReview who ranked us amongst their top 20 CMS ECM solutions prociders of 2016. We are building products that are available in various cloud ",
+    "zorang.solution":
+      "Zorang provides Product Information/Content Management (PIM/PCM), Web Content Management(WCM), B2B eCommerce, B2C eCommerce, Cloud Integration, Mobile Development and AI solutions.",
+    "zorang.teamSize":
+      "Our employee strength has been growing rapidly, we have approximately doubled in size every six months.",
+    "zorang.Technology":
+      "Zorang is a multi-faceted systems integrator and development agency. We have expertise in eCommerce solution offerings like Demandware (SFCC), SAP Hybris,  WCS, Magento etc, AI Solutions, React/Native Mobile development for Android and IOS, Java, PHP, Python etc., development.",
+    "zorang.username-input": "Hey! $name How can I help you?",
+    "zorang.webcontent":
+      "Zorang has extensive experience in Enterprise Content Management and specifically Web Content Management and was founded by two ECM industry veterans.",
+    "zorang.whatIsMyName": "Your name is " + this.User + " Right!",
+    "zorang.whatisyourname":
+      "My name is Zora! I am a chat bot. I am backed by Machine Learning."
+  }
+};
